69dd01b673498f9d1614674c641a327f5db142ce
ade yahya <adeyahyaprasetyo@gmail.com>
2018-02-20 22:53:28 +0700

    basic functionality

---
+++ index.js
@@ -0,0 +1,22 @@
+const intervals = {};
+const ticker = setInterval(() => {
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const time = `${hour}:${minute}`;
+
+  if (intervals[time]) {
+    intervals[time]();
+  }
+}, 30000);
+
+const jadwalin = (callback) => {
+  this.callback = callback;
+  return {
+    setiap: (time) => {
+      intervals[time] = this.callback;
+    }
+  }
+}
+
+module.exports = jadwalin;
