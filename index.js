90730ecb2554660075ede229dc01f750d2c99cb8
ade yahya <adeyahyaprasetyo@gmail.com>
2018-02-21 20:24:35 +0700

    start the clock precisely ON the 0th millisecond

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const intervals = {};
-const ticker = setInterval(() => {
-  const date = new Date();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
-  const time = `${hour}:${minute}`;
+const date = new Date();
+let ticker;
+const startTicker = () => {
+  ticker = setInterval(() => {
+    const date = new Date();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const time = `${hour}:${minute}`;
 
-  if (intervals[time]) {
-    intervals[time]();
-  }
-}, 40000);
+    if (intervals[time]) {
+      intervals[time]();
+    }
+  }, 60000);
+}
+
+// Make sure we start the clock precisely ON the 0th millisecond
+setTimeout(() => {
+  startTicker();
+}, Math.ceil((+date / 1000) * 1000 - +date))
 
 const jadwalin = (callback) => {
   this.callback = callback;
