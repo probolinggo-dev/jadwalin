edc478269324f165c919e93f9f52b40b0116cb09
ade yahya <adeyahyaprasetyo@gmail.com>
2018-03-14 15:15:35 +0700

    normalize time, added validation

--- index.js
+++ index.js
@@ -12,20 +12,32 @@ const startTicker = () => {
       intervals[time]();
     }
   }, 60000);
-}
+};
 
 // Make sure we start the clock precisely ON the 0th millisecond
 setTimeout(() => {
   startTicker();
-}, Math.ceil((+date / 1000) * 1000 - +date))
+}, Math.ceil((+date / 1000) * 1000 - +date));
 
-const jadwalin = (callback) => {
+const jadwalin = (callback = null) => {
   this.callback = callback;
   return {
+    destroy: () => clearInterval(ticker),
     setiap: (time) => {
-      intervals[time] = this.callback;
+      if (typeof time !== 'string')
+        throw new Error('time should be string with format hh:mm');
+
+      const splittedTime = time.split(':');
+      const hour = parseInt(splittedTime[0]);
+      const minute = parseInt(splittedTime[1]);
+
+      if (isNaN(hour) || isNaN(minute))
+        throw new Error('time should be string with format hh:mm');
+
+      const normalizedTime = `${hour}:${minute}`;
+      intervals[normalizedTime] = this.callback;
     }
-  }
-}
+  };
+};
 
 module.exports = jadwalin;
